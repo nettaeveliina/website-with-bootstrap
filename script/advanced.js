22d17591d72d91dbff52af61b81593a0dfa379c1
Netta <c1nine00@students.oamk.fi>
2022-05-02 21:31:34 +0300

    I made new contact page

---
+++ script/advanced.js
@@ -0,0 +1,14 @@
+
+let button = document.getElementById("button1");
+
+
+button.addEventListener("mouseover", doSome);
+button.addEventListener("mouseleave", doSome);
+
+button.addEventListener("mouseup", doSome);
+
+
+let div = document.getElementById("header");
+
+
+div.addEventListener("click", doSome);
