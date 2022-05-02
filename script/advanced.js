228e6eecb14609497a3371497579b2587036aad1
Netta <c1nine00@students.oamk.fi>
2022-05-02 21:58:20 +0300

    added some advancedJava js

--- script/advanced.js
+++ script/advanced.js
@@ -1,4 +1,35 @@
 
+
+
+
+let artC = document.getElementById("java");
+
+artC.style.color = "white";
+artC.style.backgroundColor = "darkgreen";
+artC.style.fontSize = "70px";
+artC.style.fontFamily = "Helvetica";
+
+
+let button1 = document.getElementById("button1");
+
+button1.style.color = "black";
+button1.style.backgroundColor = "lightpink";
+button1.style.fontSize = "20px";
+button1.style.fontFamily = "'BPscript', sans-serif";
+
+
+let button2 = document.getElementById("button2");
+
+button2.style.color = "black";
+button2.style.backgroundColor = "yellow";
+button2.style.fontSize = "20px";
+button2.style.padding = "15px";
+button2.style.margin = "20px";
+button2.style.fontWeight = "bold"
+button2.style.fontFamily = "'BPscript', sans-serif";
+
+background: linear-gradient
+
 let button = document.getElementById("button1");
 
 
