3711142b4f9f4d58df1140525dbaa290e21eb625
Netta <c1nine00@students.oamk.fi>
2022-05-05 01:05:25 +0300

    Added some texth in index and contact html and made some changes in css  and js

--- script/advanced.js
+++ script/advanced.js
@@ -24,7 +24,7 @@ button1.style.color = "white";
 button1.style.backgroundColor = "black";
 button1.style.fontSize = "20px";
 button1.style.fontFamily = "'BPscript', sans-serif";
-button1.style.marginLeft = "80px"
+button1.style.marginLeft = "40px"
 button1.style.padding = "10px";
 
 let button2 = document.getElementById("button2");
@@ -32,8 +32,6 @@ let button2 = document.getElementById("button2");
 button2.style.color = "black";
 button2.style.backgroundColor = "gray";
 button2.style.fontSize = "20px";
-
-
 button2.style.margin = "20px";
 button2.style.fontWeight = "bold"
 button2.style.fontFamily = "'BPscript', sans-serif";
@@ -43,4 +41,6 @@ let text = document.getElementById("h1");
 text.style.color = "black";
 text.style.fontSize = "50px";
 text.style.fontFamily = "'BPscript', sans-serif";
-p3.innerHTML ="This use to just say that this page is only for Advanced JavaScript" + "<br>" 
+p3.innerHTML ="This use to say that this page is only for Advanced JavaScript" + "<br>" 
+text.classList.toggle("shading");
+
