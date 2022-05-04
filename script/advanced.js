47bd767c4037167ed1659c66b2fd2e74ccdf8807
Netta <c1nine00@students.oamk.fi>
2022-05-04 19:56:52 +0300

    added some new advancedjava manipulation

--- script/advanced.js
+++ script/advanced.js
@@ -1,3 +1,4 @@
+
 fetch('https://yesno.wtf/api')
 .then(response => response.json())
 .then(
@@ -10,30 +11,36 @@ fetch('https://yesno.wtf/api')
 
 let api = document.getElementById("print-here");
 
-
-
-
 let artC = document.getElementById("java");
 
 artC.style.color = "white";
-artC.style.backgroundColor = "darkgreen";
-artC.style.fontSize = "70px";
+artC.style.backgroundColor = "rgba(0, 60, 36, 0.603)";
+artC.style.fontSize = "30px";
 artC.style.fontFamily = "Helvetica";
 
 let button1 = document.getElementById("button1");
 
-button1.style.color = "black";
-button1.style.backgroundColor = "lightpink";
+button1.style.color = "white";
+button1.style.backgroundColor = "black";
 button1.style.fontSize = "20px";
 button1.style.fontFamily = "'BPscript', sans-serif";
+button1.style.marginLeft = "80px"
+button1.style.padding = "10px";
 
 let button2 = document.getElementById("button2");
 
 button2.style.color = "black";
-button2.style.backgroundColor = "yellow";
+button2.style.backgroundColor = "gray";
 button2.style.fontSize = "20px";
-button2.style.padding = "15px";
+
+
 button2.style.margin = "20px";
 button2.style.fontWeight = "bold"
 button2.style.fontFamily = "'BPscript', sans-serif";
 
+let text = document.getElementById("h1");
+
+text.style.color = "black";
+text.style.fontSize = "50px";
+text.style.fontFamily = "'BPscript', sans-serif";
+p3.innerHTML ="This use to just say that this page is only for Advanced JavaScript" + "<br>" 
