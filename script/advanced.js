746dbfe0ea52c134e26f754490e09bc0b2f59871
Netta <c1nine00@students.oamk.fi>
2022-05-14 16:32:06 +0300

    added some events and changes in andvanced js

--- script/advanced.js
+++ script/advanced.js
@@ -8,7 +8,7 @@ artC.style.backgroundColor = "rgba(0, 60, 36, 0.603)";
 artC.style.fontSize = "30px";
 artC.style.fontFamily = "Helvetica";
 
-//Button1 (Videos)
+//Button1 (Home)
 
 let button1 = document.getElementById("button1");
 
@@ -16,8 +16,8 @@ button1.style.color = "white";
 button1.style.backgroundColor = "black";
 button1.style.fontSize = "20px";
 button1.style.fontFamily = "'BPscript', sans-serif";
-button1.style.marginLeft = "180px"
-button1.style.padding = "10px";
+button1.style.marginLeft = "510px"
+
 
 //Button2 (Instagram)
 
@@ -30,7 +30,8 @@ button2.style.margin = "20px";
 button2.style.fontWeight = "bold"
 button2.style.fontFamily = "'BPscript', sans-serif";
 
-//Button2 (Rest Api)
+
+//Button3 (Rest Api)
 
 let button3 = document.getElementById("button3");
 
@@ -48,8 +49,38 @@ let text = document.getElementById("h1");
 
 text.style.color = "black";
 text.style.fontSize = "50px";
+text.style.textAlign  = "center";
 text.style.fontFamily = "'BPscript', sans-serif";
-p3.innerHTML ="I manipulated this text..." + "<br>" 
+text.style.marginTop = "20px"
+
 text.classList.toggle("shading");
-p3.style.marginLeft = "200px"
+
+//p
+let p = document.getElementById("p3")
+
+p3.innerHTML ="I manipulated this text..." + "<br>" 
+p.style.textAlign  = "center";
+p.style.fontFamily = "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+p.style.fontSize = "40px"
+
+
+//Image Event (Colors and text change when you click the buttons)
+
+function before() {
+    document.getElementById("image1")
+    .src="../images/java_page_image.jpg";
+    document.getElementById("image2")
+    .src="../images/java_page_image2.jpg";
+    document.getElementById("message")
+    .innerHTML="One Foggy Evening...Cool";
+}
+function afterr() {
+    document.getElementById("image1")
+    .src="../images/java_page_image3.jpg";
+    document.getElementById("image2")
+    .src="../images/java_page_image4.jpg";
+    document.getElementById("message")
+    .innerHTML="One Foggy Evening...Warm";
+}
+
 
